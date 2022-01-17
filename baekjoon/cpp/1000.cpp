7c278500411d3d4211483c53c33a9383619a8b96
송성엽 <ssyoub0327@daum.net>
2022-01-17 12:19:07 +0000

    study

---
+++ baekjoon/cpp/1000.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<a+b;
+}
+
+/*
+간단하게 더하기를 하는 문제
+*/
--- main.cpp
+++ main.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-class Circle{
-private:
-	int radius;
-public:
-	Circle(Circle &c);
-	Circle(){radius=1;}
-	Circle(int radius){this->radius = radius;}
-	double getArea(){return 3.14*radius*radius;}
-};
-
-Circle::Circle(Circle& c){
-	this->radius = c.radius;
-	cout<<"복사 생성자 실행 radius = "<<radius<<endl;
-}
-
 int main(){
-	Circle src(30);
-	Circle dest(src);
-
-	cout<<"원본의 면적 = "<<src.getArea() <<endl;
-	cout<<"사본의 면적 = "<<dest.getArea()<<endl;
+	float a,b,c;
+	float x;
+	cin>>a>>b>>c;
+	x = a/sqrt(pow(b,2)+pow(c,2));
+	cout<<floor(x*b)<<' '<<floor(x*c);
+	return 0;
 }
