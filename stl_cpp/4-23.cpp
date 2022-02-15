46441a68c1223b63799f5569370b81c7ef7e2488
Song-Sungyoub <ssyoub0327@daum.net>
2022-02-15 13:05:05 +0900

    hello

---
+++ stl_cpp/4-23.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T1, typename T2>
+struct Pair{
+	T1 first;
+	T2 second;
+	Pair(const T1& ft, const T2& sd): first(ft), second(sd){}
+};
+
+int main(){
+	Pair<int, int>
+}
