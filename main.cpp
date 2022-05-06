6dd25779af016223f98a45f73e33728c3a8dd92e
송성엽 <ssyoub0327@daum.net>
2022-05-06 11:51:08 +0000

    3문제 풀이

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main(){
-	float a,b,c;
-	float x;
-	cin>>a>>b>>c;
-	x = a/sqrt(pow(b,2)+pow(c,2));
-	cout<<floor(x*b)<<' '<<floor(x*c);
-	return 0;
-}
+run = "g++ -o main main.cpp;./main"
